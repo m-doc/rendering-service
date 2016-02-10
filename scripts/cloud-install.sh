40e3fda1826867f2c9ef407212042ab843de6330
Frank S. Thomas <frank@timepit.eu>
2016-02-10 21:27:36 +0100

    Fix

--- scripts/cloud-install.sh
+++ scripts/cloud-install.sh
@@ -3,7 +3,7 @@
 sleep 30s # give Bintray time to update the repository
 
 chmod 600 cloud-identity
-ssh -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST \
+ssh -q -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST \
   "export DEBIAN_FRONTEND=noninteractive && \
   sudo apt-get-update-only -y --force-yes m-doc && \
   sudo apt-get -y --force-yes install rendering-service"

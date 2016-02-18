5807f863a5ceb420ed283bd1a65bdf11a71bedee
Frank S. Thomas <frank@timepit.eu>
2016-02-18 21:26:52 +0100

    Change Debian package name

--- scripts/cloud-install.sh
+++ scripts/cloud-install.sh
@@ -5,4 +5,4 @@ sleep 40s # give Bintray time to update the repository
 chmod 600 cloud-identity
 ssh -q -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST "\
   sudo DEBIAN_FRONTEND=noninteractive apt-get-update-only -y --force-yes m-doc && \
-  sudo DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install rendering-service"
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install mdoc-rendering-service"

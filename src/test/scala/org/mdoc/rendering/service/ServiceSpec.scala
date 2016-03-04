419de79b91b88b7e8383fea36841be78002dd0d7
Frank S. Thomas <frank@timepit.eu>
2016-03-04 23:38:11 +0100

    Test /render without specifying an URL

--- src/test/scala/org/mdoc/rendering/service/ServiceSpec.scala
+++ src/test/scala/org/mdoc/rendering/service/ServiceSpec.scala
@@ -15,6 +15,11 @@ object ServiceSpec extends Properties("Service") {
     Service.route.run(Request()).run.status ?= NotFound
   }
 
+  property("route: /render/pdf/test.pdf") = secure {
+    val req = Request(uri = Uri(path = "/render/pdf/test.pdf"))
+    Service.route.run(req).run.status ?= BadRequest
+  }
+
   property("route: /version") = secure {
     Service.route.run(Request(uri = Uri(path = "/version"))).run.status ?= Ok
   }
